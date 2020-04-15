eeed26b467a00b9e7ca046d97c08712904015903
okyang <owenyang123@gmail.com>
2020-04-14 21:36:35 -0700

    update api route name

--- ZotBinsAPI.py
+++ ZotBinsAPI.py
@@ -26,7 +26,7 @@ def allowed_file(filename):
     """
     return '.' in filename and filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/image/add', methods=['GET','POST'])
+@app.route('/observation/add/image', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
